33e4d3ff89afdc991b5f501382bb70983cc904cf
Michael Camerlynck <camerlynck.michael@gmail.com>
2023-08-16 18:51:10 +0200

    Update settings.py

--- PORTFOLIO/settings.py
+++ PORTFOLIO/settings.py
@@ -75,6 +75,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    'django.middleware.locale.LocaleMiddleware',
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -144,12 +145,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-
-# LANGUAGES = [
-#     ('en', 'English'),
-#     ('nl', 'Dutch'),
-#     ('fr', 'france'),
-# ]
 
 LOCALE_PATHS = [
     os.path.join(BASE_DIR, "locale")
